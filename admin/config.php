ac7103ebc1ac1708223253989cfbfa5c19d3e4cb
manish <manish@onfees.com>
2026-01-18 22:39:31 +0530

    Force push config files for production deployment

---
+++ admin/config.php
@@ -0,0 +1,38 @@
+<?php
+date_default_timezone_set("Asia/Kolkata");
+// HTTP
+define('HTTP_SERVER', 'http://5.161.114.135/phiroz_2020/admin/');
+define('HTTP_CATALOG', 'http://5.161.114.135/phiroz_2020/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://5.161.114.135/phiroz_2020/admin/');
+define('HTTPS_CATALOG', 'http://5.161.114.135/phiroz_2020/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/phiroz_2020/admin/');
+define('DIR_SYSTEM', '/var/www/html/phiroz_2020/system/');
+define('DIR_DATABASE', '/var/www/html/phiroz_2020/system/database/');
+define('DIR_LANGUAGE', '/var/www/html/phiroz_2020/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/phiroz_2020/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/phiroz_2020/system/config/');
+define('DIR_IMAGE', '/var/www/html/phiroz_2020/image/');
+define('DIR_CACHE', '/var/www/html/phiroz_2020/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/phiroz_2020/download/');
+define('DIR_LOGS', '/var/www/html/phiroz_2020/system/logs/');
+define('DIR_CATALOG', '/var/www/html/phiroz_2020/catalog/');
+
+define('DOCTORNAME1', 'Dr. Phiroz Khambatta');
+define('DOCTORNAME11', 'Dr. P.T Khambatta');
+define('DOCTORNAME2', 'Dr. Leila Fernandes');
+define('DOCTORNAME22', 'Dr. L.M Fernandes');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+//define('DB_HOSTNAME', '127.0.0.1');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'enats');
+define('DB_PASSWORD', 'Enats@2021');
+//define('DB_DATABASE', 'db_phiroz_2020');
+define('DB_DATABASE', 'db_phiroz_2020_bk_2022_10_25');
+define('DB_PREFIX', 'oc_');
+?>
